abb5582c42840cd8a1da39c106b55cc9654ac3a1
georgeben <msdcconnect@gmail.com>
2019-02-22 12:57:39 +0100

    Translate words with the Yandex API

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -4,7 +4,7 @@
       <v-layout wrap>
         <v-flex xs12 md4>
           <v-text-field
-            v-model="wordToTranslate"
+            v-model="textToTranslate"
             :rules="nameRules"
             label="Enter word to translate"
             required
@@ -18,7 +18,7 @@
             ></v-select>
         </v-flex>
         <v-flex xs12 md4>
-            <v-btn class="cyan white--text" depressed block large :loading="loadingTranslation">Translate</v-btn>
+            <v-btn class="cyan white--text" depressed block large :loading="showLoading" @click="translateText">Translate</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -26,11 +26,15 @@
 </template>
 
 <script>
+import axios from 'axios';
+const apiUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+const apiKey = 'trnsl.1.1.20190221T144049Z.883b3d4ed8b6b562.8b58243bd33572951d04ad3da4dbff1674de7426';
+
   export default {
     data: () => ({
       valid: false,
-      wordToTranslate: '',
-      language:'',
+      textToTranslate: '',
+      language:'ru',
       nameRules: [
         v => !!v || 'Please enter a word to translate'
       ],
@@ -38,7 +42,30 @@
           {text: 'Russian', value: 'ru'},
           {text: 'Greek', value: 'el'}
       ],
-      loadingTranslation: false
-    })
+      showLoading: false
+    }),
+    methods: {
+        translateText(){
+            if(this.valid){
+                console.log("About to translate")
+                this.showLoading = true
+                axios.get(apiUrl, {
+                    params: {
+                        key: apiKey,
+                        text: this.textToTranslate,
+                        lang: this.language
+                    }
+                })
+                .then(res => {
+                    this.showLoading = false
+                    console.log("Response from yandex", res.data.text)
+                })
+                .catch(err => {
+                    this.showLoading = false
+                    console.log("An error occured", err)
+                })
+            }
+        }
+    }
   }
 </script>
