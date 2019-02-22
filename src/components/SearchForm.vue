423c0b38d576f3aafaf5f5d749b9c913d59ca654
georgeben <msdcconnect@gmail.com>
2019-02-22 12:45:14 +0100

    Create search form

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,43 +1,24 @@
 <template>
   <v-form v-model="valid">
-    <v-container>
-      <v-layout>
-        <v-flex
-          xs12
-          md4
-        >
+    <v-container grid-list-md>
+      <v-layout wrap>
+        <v-flex xs12 md4>
           <v-text-field
-            v-model="firstname"
+            v-model="wordToTranslate"
             :rules="nameRules"
-            :counter="10"
-            label="First name"
+            label="Enter word to translate"
             required
           ></v-text-field>
         </v-flex>
-
-        <v-flex
-          xs12
-          md4
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
+        <v-flex xs12 md4>
+            <v-select
+            v-model="language"
+            :items="languageOptions"
+            label="Language"
+            ></v-select>
         </v-flex>
-
-        <v-flex
-          xs12
-          md4
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+        <v-flex xs12 md4>
+            <v-btn class="cyan white--text" depressed block large :loading="loadingTranslation">Translate</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -48,17 +29,16 @@
   export default {
     data: () => ({
       valid: false,
-      firstname: '',
-      lastname: '',
+      wordToTranslate: '',
+      language:'',
       nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters'
+        v => !!v || 'Please enter a word to translate'
       ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ]
+      languageOptions: [
+          {text: 'Russian', value: 'ru'},
+          {text: 'Greek', value: 'el'}
+      ],
+      loadingTranslation: false
     })
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-
+  <SearchForm />
 </template>
 
 <script>
-
+import SearchForm from '@/components/SearchForm'
 
   export default {
     components: {
-      
+      SearchForm,
     }
   }
 </script>
